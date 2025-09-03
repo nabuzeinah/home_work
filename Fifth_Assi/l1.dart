b5a40792e4411e7c3bfcac48cf187505c9596f6b
nidaa <nidaa.1988@gmail.com>
2025-09-03 18:44:37 +0300

    leetcode

--- Fifth_Assi/l1.dart
+++ Fifth_Assi/l1.dart
@@ -1,30 +1,19 @@
 void main() {
-  List<int> nums = [0, 4, 3, 0];
-  int target = 0;
+  List<int> nums = [3, 2, 4];
+  int target =6;
   print(twoSum(nums, target));
 }
 
-List<int> twoSum(List<int> nums, int target) {
+List<int>? twoSum(List<int> nums, int target) {
   int sum = 0;
-  int index1 = 0;
-  int index2 = 0;
-  for (int i = 1; i < nums.length; i++) {
-    for (int j = 0; j < i; j++) {
-      sum = nums[i] + nums[j];
-      print('${nums[j]} + ${nums[i]} = $sum');
-      
-      if ((index1 != index2 ) && (sum == target)) {
-        index1 = j;
-        index2 = i;
-        break;
-      } else {
-        sum = 0;
+
+  for (int i = 0; i < nums.length; i++) {
+    for (int j = i + 1; j < nums.length; j++) {
+      sum = nums[j] + nums[i];
+    //  print('nums[$i] + nums[$j]');
+      if (sum == target) {
+        return [i, j];
       }
     }
-    if ((index1 != index2) && (sum == target)) {
-      break;
-    }
   }
-
-  return [index1, index2];
 }
